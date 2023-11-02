879d15a7fb6708f5932f0d1f1bc4e7008eea3902
netcam22 <annettelesage@btinternet.com>
2023-11-02 14:45:14 +0000

    refactor: combine split and reduce

--- src/roman.ts
+++ src/roman.ts
@@ -1,37 +1,33 @@
 import { ROM_NUM } from "../src/roman.types";
 
 export function romanToDecimal(roman: string): number {
-  //split - string to array - eg M C X C I
-  const charArray = roman.split("");
+  const charArray = roman
+    .split("")
+    .reduce(
+      (
+        romArray: Array<string>,
+        char: string,
+        i: number,
+        charArray: Array<string>
+      ): Array<string> => {
+        // if this one and the next one combined make a double, don't count it
+        // if this one and the previous one is a double, use this one and the previous one
+        console.log(ROM_NUM[`${charArray[i - 1]}${char}`]);
+        const thisNumeral =
+          i > 0 && ROM_NUM[`${charArray[i - 1]}${char}`]
+            ? "skip"
+            : ROM_NUM[`${char}${charArray[i + 1]}`]
+            ? `${char}${charArray[i + 1]}`
+            : char;
+        return thisNumeral !== "skip"
+          ? [...romArray, thisNumeral]
+          : [...romArray];
+      },
+      []
+    );
   console.log(charArray);
-  //reduce - turn MCXCU into M C XC I, adding values into new array
-  //check if double, ie. this character and next character together is a roman numeral
-  //check if previous value is single add next character to array, otherwise miss it
-  const reducedArray = charArray.reduce(
-    (
-      romArray: Array<string>,
-      char: string,
-      i: number,
-      charArray: Array<string>
-    ): Array<string> => {
-      // if this one and the next one combined make a double, don't count it
-      // if this one and the previous one is a double, use this one and the previous one
-      console.log(ROM_NUM[`${charArray[i - 1]}${char}`]);
-      const thisNumeral =
-        i > 0 && ROM_NUM[`${charArray[i - 1]}${char}`]
-          ? "skip"
-          : ROM_NUM[`${char}${charArray[i + 1]}`]
-          ? `${char}${charArray[i + 1]}`
-          : char;
-      return thisNumeral !== "skip"
-        ? [...romArray, thisNumeral]
-        : [...romArray];
-    },
-    []
-  );
-  console.log(reducedArray);
   // reduce again to count up values
-  const decimal = reducedArray.reduce(
+  const decimal = charArray.reduce(
     (decCount: number, charString: string): number => {
       return (decCount += ROM_NUM[charString]);
     },
